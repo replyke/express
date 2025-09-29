d66af41bea37eeab84e6f09b2a303c25d7a5d37c
Yanay Zabary <yanay.tsabary@gmail.com>
2025-09-29 14:17:39 +0300

    rename directory and export to v6

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from "./interfaces";
 export * from "./models";
 export { reportReasons, reputationScores } from "./constants";
 export { type ReportReasonKey } from "./constants";
-export { default as v5 } from "./v5";
+export { default as v6 } from "./v6";
 export { setCoreConfig } from "./config";
