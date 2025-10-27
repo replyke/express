0613a178953f05ef2fcdcfc4b52274fa9a93d3fe
Yanay Zabary <yanay.tsabary@gmail.com>
2025-10-27 16:06:08 +0200

    updae mentions

--- src/interfaces/IMention.ts
+++ src/interfaces/IMention.ts
@@ -1,4 +1,5 @@
 export default interface IMention {
   id: string; // User ID of the mentioned user
+  foreignId?: string | null; // User foreign ID of the mentioned user
   username: string; // Current username for easy lookup
 }
